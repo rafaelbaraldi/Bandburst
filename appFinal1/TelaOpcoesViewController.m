93531f0cdb3f0c577e59eb9a29d913308f3925dc
rafaelbaraldi <rafael.baraldi@novas.local>
2014-09-18 15:37:51 -0300

    facebook
    login

--- appFinal1/TelaOpcoesViewController.m
+++ appFinal1/TelaOpcoesViewController.m
@@ -28,6 +28,13 @@
     [super viewDidLoad];
     
     [[self navigationItem] setTitle:@"Opções"];
+    
+    FBLoginView* loginView = [[FBLoginView alloc] initWithReadPermissions:@[@"public_profile", @"email", @"user_friends"]];
+    CGRect frame = loginView.frame;
+    frame.origin.y = 400;
+    loginView.frame = frame;
+    //    loginView.frame = CGRectOffset(loginView.frame, (self.view.center.x - (loginView.frame.size.width / 2)), 150);
+    [self.view addSubview:loginView];
 }
 
 - (void)didReceiveMemoryWarning{
@@ -39,6 +46,39 @@
 }
 
 - (IBAction)btnEcontrarAmigos:(id)sender {
+    //    [FBRequestConnection startForMyFriendsWithCompletionHandler:
+    //     ^(FBRequestConnection *connection, id<FBGraphUser> friends, NSError *error)
+    //     {
+    //         if(!error){
+    //             NSLog(@"%@", friends);
+    //         }
+    //         else{
+    //             NSLog(@"%@", error);
+    //         }
+    //     }];
+    
+    
+    FBRequest* friendsRequest = [FBRequest requestForMyFriends];
+    [friendsRequest startWithCompletionHandler: ^(FBRequestConnection *connection,
+                                                  NSDictionary* result,
+                                                  NSError *error)
+     {
+         NSMutableArray* friendsArray = [result objectForKey:@"data"];
+         NSLog(@"%@", result);
+         NSObject *friend =  [friendsArray objectAtIndex:0];
+         NSLog(@"%@", [friend valueForKey:@"name"]);
+         
+     }];
+    
+    FBFriendPickerViewController* fbvc = [[FBFriendPickerViewController alloc] init];
+    [fbvc loadData];
+    
+    [fbvc presentModallyFromViewController:self animated:YES handler:^(FBViewController* innerSender, BOOL donePressed)
+     {
+         if(!donePressed){
+             return;
+         }
+     }];
 }
 
 - (IBAction)btnSair:(id)sender {
